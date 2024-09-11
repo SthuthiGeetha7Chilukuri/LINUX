6188e2d32c6227d532f308bb6988c5141c83d478
SthuthiGeetha7Chilukuri <sthuthigeetha1@gmail.com>
2024-09-11 12:19:42 +0530

    system call in system programming

---
+++ system_call.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+	printf("Hai......\n");
+	system("CAL");		//sh -> cal
+	printf("hello....\n");
+	system("ls");		//sh->ls
+	printf("Sthuthi..\n");
+//	while(1);
+}
